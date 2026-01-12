757b43ab0c030cb4eef0234911b2b8086e6a6467
RoAntoo <157826491+RoAntoo@users.noreply.github.com>
2026-01-12 11:21:51 -0300

    Carga proyecto general

---
+++ src/main/java/com/ramaccioni/api_clean_arch/ApiCleanArchApplication.java
@@ -0,0 +1,13 @@
+package com.ramaccioni.api_clean_arch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCleanArchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCleanArchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ramaccioni/api_clean_arch/ApiCleanArchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramaccioni.api_clean_arch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCleanArchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
